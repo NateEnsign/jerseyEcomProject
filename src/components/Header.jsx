f92d5f44dd3f8487e2d1992be651ada9449bb07f
Nate Ensign <dixienate@gmail.com>
2023-01-16 13:08:09 -0700

    more styling done on homescreen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
-import logo from '../assets/logo.png';
+
+
 
 const Header = () => {
   return (
     <div>
       <header className={styles.header}>
-        <div>src={logo}</div>
+        <h2>Jersey Town</h2>
         <ul>
           <Link to="/">
             <button>Home</button>
